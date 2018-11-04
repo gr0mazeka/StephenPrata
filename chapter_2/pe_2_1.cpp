8c4000809549b09af5f33289f1a6b19ab68523c4
gr0mazeka <kmvpipl@yandex.ru>
2018-11-05 00:22:29 +0300

    Add solutions

--- chapter_2/pe_2_1.cpp
+++ chapter_2/pe_2_1.cpp
@@ -1,4 +1,4 @@
-// ---------- Prata 2.1 ----------
+// ---------- Chapter 2.1 ----------
 /*
 	Write a C++ program that displays your name and address (or if you value your
 	privacy, a fictitious name and address).
