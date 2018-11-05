b35f06f08cf21d664fc9c11cb29674deac7f0b91
gr0mazeka <kmvpipl@yandex.com>
2018-11-05 18:02:35 +0300

    Add solutions

--- chapter_2/pe_2_2.cpp
+++ chapter_2/pe_2_2.cpp
@@ -5,11 +5,12 @@
  */
 #include <iostream>
 int converts(int f){ return f*220; }
-int mail()
+int main()
 {
+    std::cout << "Enter furlongs: ";
     int fur;
-    std::cin >> "Enter furlongs: " >> fur;
-    std::cout << fur << " furlongs = " << converts(fur) << " yards" std::endl;
+    std::cin >> fur;
+    std::cout << fur << " furlongs = " << converts(fur) << " yards" << std::endl;
     std::cin.get();
     return 0;
 }
