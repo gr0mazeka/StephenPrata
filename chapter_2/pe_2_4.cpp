f4a5f6aa2b3b15b55ab1fe10a6fb1b17edeaaf42
gr0mazeka <kmvpipl@yandex.com>
2018-11-05 22:10:01 +0300

    add solution

---
+++ chapter_2/pe_2_4.cpp
@@ -0,0 +1,19 @@
+//  ---------- Programming Exercises 2.4 ----------
+/*
+    Write a program that asks the user to enter his or her age.The program then should
+    display the age in months:
+    Enter your age: 29
+    Your age in months is 384.
+*/
+#include <iostream>
+int converts(int a){ return a*12; }
+int main()
+{
+    std::cout << "Enter your age: ";
+    int age;
+    std::cin >> age;
+    std::cout << "Your age in months is " << converts(age) << std::endl;
+    std::cin.get();
+    std::cin.get();
+    return 0;
+}
