6254cae117dbc705d3820c67983e1489d7343a69
gr0mazeka <kmvpipl@yandex.com>
2018-11-06 22:04:25 +0300

    add solution

--- chapter_2/sol-2-5.cpp
+++ chapter_2/sol-2-5.cpp
@@ -5,8 +5,8 @@
     Celsius temperature value as an argument and then returns the equivalent 
     Fahrenheit value. The program should request the Celsius value as input from
     the user and display the result, as shown in the following code:
-    Please enter a Celsius value: 20
-    20 degrees Celsius is 68 degrees Fahrenheit.
+        Please enter a Celsius value: 20
+        20 degrees Celsius is 68 degrees Fahrenheit.
     For reference, here is the formula for making the conversion:
     Fahrenheit = 1.8 × degrees Celsius + 32.0
 */
