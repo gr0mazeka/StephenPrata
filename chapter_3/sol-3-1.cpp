5f62ce48777f4634c8a623886c1a951468047216
gr0mazeka <kmvpipl@yandex.com>
2018-11-09 23:20:17 +0300

    add solution

--- chapter_3/sol-3-1.cpp
+++ chapter_3/sol-3-1.cpp
@@ -1,5 +1,5 @@
-//  ---------- S.Prata C++ Primer Plus 6th ed. ----------
-//  ------------- Programming Exercises 3.1 -------------
+//  --------------------- S.Prata C++ Primer Plus 6th ed. ----------------------
+//  ------------------------ Programming Exercises 3.1 -------------------------
 /*
     Write a short program that asks for your height in integer inches and then
     converts your height to feet and inches. Have the program use the underscore
@@ -7,21 +7,14 @@
     constant to represent the conversion factor.
 */
 #include <iostream>
-const int FT = 12;  // feet = 12 inches
-void convert(int i);
 int main()
 {
-    std::cout << "Enter your heit in integer inches: ___\b\b\b";
-    int inch;
-    std::cin >> inch;
-    convert(inch);
-    std::cin.get();
-    std::cin.get();
-    return 0;
-}
-void convert(int i)
-{
-    int feet = i/FT;
-    int inch = i%FT;
+    const int INCH_FT = 12;     // 1 ft = 12 inches
+    std::cout << "Enter your height in integer inches: ___\b\b\b";
+    int height;
+    std::cin >> height;
+    int feet = height/INCH_FT;
+    int inch = height%INCH_FT;
     std::cout << "Yur height is "<< feet << " feets and "<< inch << " inches\n";
+    return 0;
 }
