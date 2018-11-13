f93d6bdc6693dd4593acad2425e2580096bf245b
gr0mazeka <kmvpipl@yandex.com>
2018-11-13 20:16:13 +0300

    add solution

---
+++ chapter_6/sol-6-03.cpp
@@ -0,0 +1,26 @@
+//  --------------------- S.Prata C++ Primer Plus 6th ed. ----------------------
+//  ------------------------ Programming Exercises 6.3 -------------------------
+
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Please enter one of the following choices:\n"
+            " c) carnivore p) pianist\n"
+            " t) tree g) game\n";
+    char ch;
+    cin >> ch;
+    while(ch !='c' && ch !='p' && ch !='t' && ch !='g')
+    {
+        cout << "Please enter a c, p, t, or g: ";
+        cin >> ch;
+    }
+    switch(ch)
+    {
+        case 'c' : cout << "Carnivore\n"; break;
+        case 'p' : cout << "Pianist\n"; break;
+        case 't' : cout << "A maple is a tree.\n"; break;
+        case 'g' : cout << "Game\n"; break;
+    }
+    return 0;
+}
---
+++ chapter_6/sol-6-04.cpp
@@ -0,0 +1,60 @@
+//  --------------------- S.Prata C++ Primer Plus 6th ed. ----------------------
+//  ------------------------ Programming Exercises 6.4 -------------------------
+
+#include <iostream>
+const int strsize = 20;
+const int SIZE = 5;
+struct bop {
+    char fullname[strsize];
+    char title[strsize];
+    char bopname[strsize];
+    int preference;
+};
+int main()
+{
+    using namespace std;
+    bop arrbop[SIZE]{
+        {"Wimp Macho", "Junior Programmer", "MIPS", 0},
+        {"Raki Rhodes", "Analyst Trainee", "LOOP", 1},
+        {"Celia Laiter", "Broker", "STAR", 2},
+        {"Hoppy Hipman", "Engineer", "MAGIC", 1 },
+        {"Pat Hand", "Electrician", "SUN", 2 }
+    };
+    cout << "Benevolent Order of Programmers Report\n"
+            " a. display by name\t b. display by title\n"
+            " c. display by bopname\t d. display by preference\n"
+            " q. quit\n";
+    cout << "Enter your choice: ";
+    char ch;
+    cin >> ch;
+    while(ch !='q')
+    {
+        switch (ch)
+        {
+            case 'a': {
+                    for(int i = 0; i < SIZE; i++)
+                        cout << arrbop[i].fullname << endl;
+                    break; }
+            case 'b': {
+                    for(int i = 0; i < SIZE; i++)
+                        cout << arrbop[i].title << endl;
+                    break; }
+            case 'c': {
+                    for(int i = 0; i < SIZE; i++)
+                        cout << arrbop[i].bopname << endl;
+                    break; }
+            case 'd': {
+                    for(int i = 0; i < SIZE; i++)
+                        switch (arrbop[i].preference)
+                        {
+                            case 0: cout << arrbop[i].fullname << endl; break;
+                            case 1: cout << arrbop[i].title << endl; break;
+                            case 2: cout << arrbop[i].bopname << endl; break;
+                        }
+                    break; }
+        }
+        cout << "Next choice: ";
+        cin >> ch;
+    }
+    return 0;
+}
