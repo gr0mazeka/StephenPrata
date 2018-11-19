f7db7c4e1950ce5690461195da62c9daf95457fb
gr0mazeka <kmvpipl@yandex.com>
2018-11-19 09:26:02 +0300

    add solution

---
+++ chapter_7/sol-7-01.cpp
@@ -0,0 +1,14 @@
+//  --------------------- S.Prata C++ Primer Plus 6th ed. ----------------------
+//  ------------------------ Programming Exercises 7.1 -------------------------
+
+#include <iostream>
+double harmonic(double x, double y) { return (2.0*x*y)/(x+y); }
+int main()
+{
+    std::cout << "Enter pairs of numbers: (0 for terminate):\n";
+    double x, y;
+    while (std::cin >> x >> y && x != 0 && y != 0)
+        std::cout << "Harmonic (" << x << ", " << y << ") = " << harmonic(x, y)
+                  << "\nEnter pair of numbers:\n";
+    return 0;
+}
---
+++ chapter_7/sol-7-02.cpp
@@ -0,0 +1,49 @@
+//  --------------------- S.Prata C++ Primer Plus 6th ed. ----------------------
+//  ------------------------ Programming Exercises 7.2 -------------------------
+
+#include <iostream>
+const int SIZE = 10;
+int fill_ar(int ar[], int limit);
+void avrage(int ar[], int n);
+void show_ar(const int ar[], int n);
+int main()
+{
+    int result[SIZE];
+    int size = fill_ar(result, SIZE);
+    show_ar(result, size);
+    avrage(result, size);
+    return 0;
+}
+int fill_ar(int ar[], int limit)
+{
+    int i, temp;
+    std::cout << "Enter up to 10 golf scores: \n";
+    for(i = 0; i < limit; i++)
+    {
+        std::cout << "Value #" << (i + 1) << ": ";
+        std::cin >> temp;
+        if(!std::cin)
+        {
+            std::cin.clear();
+            while(std::cin.get() != '\n')
+                continue;
+            std::cout << "Bad input; input process terminated.\n";
+            break;
+        } else if (temp < 0)
+            break;
+        ar[i] = temp;
+    }
+    return i;
+}
+void avrage(int ar[], int n)
+{
+    double temp = 0.0;
+    for(int i = 0; i < n; i++)
+        temp += ar[i];
+    std::cout << "Avrage score: " << temp/n << std::endl;
+}
+void show_ar(const int ar[], int n)
+{
+    for(int i = 0; i < n; i++)
+        std::cout << "Property #" << (i + 1) << ": " << ar[i] << std::endl;
+}
