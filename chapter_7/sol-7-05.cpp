e83b94310ae0cb444e8d371e8bee12c657552390
gr0mazeka <kmvpipl@yandex.com>
2018-11-30 08:13:01 +0300

    add solution

--- chapter_7/sol-7-05.cpp
+++ chapter_7/sol-7-05.cpp
@@ -2,8 +2,19 @@
 //  ------------------------ Programming Exercises 7.5 -------------------------
 
 #include <iostream>
+unsigned long int factor(unsigned int n);
 int main()
 {
+    std::cout << "Enter value: ";
+    unsigned int size;
+    while (std::cin >> size)
+        std::cout << size << "! " << factor(size) << "\nNext value: ";
     return 0;
 }
-
+unsigned long int factor(unsigned int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n*factor(n - 1);
+}
